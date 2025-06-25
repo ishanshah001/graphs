3a816a2917846b1ff104b925e7fee26588d5e52d
is4761 <ishanpshah001@gmail.com>
2025-06-25 18:59:04 -0400

    union find

---
+++ union_and_find.py
@@ -0,0 +1,39 @@
+def initialize(n):
+    parent = list(range(n))
+    rank = [0] * n
+    return parent, rank
+
+def find(parent, x):
+    if parent[x] != x:
+        parent[x] = find(parent, parent[x])
+    return parent[x]
+
+def union(parent, rank, x, y):
+    rootX = find(parent, x)
+    rootY = find(parent, y)
+
+    # already in same component
+    if rootX == rootY:
+        return False
+    
+    if rank[rootX] < rank[rootY]:
+        parent[rootX] = rootY
+    elif rank[rootX] > rank[rootY]:
+        parent[rootY] = rootX
+    else:
+        parent[rootY] = rootX
+        rank[rootX] += 1
+
+    return True
+
+parent, rank = initialize(5)
+
+print(parent)
+print(rank)
+
+union(parent, rank, 4, 2)
+union(parent, rank, 2, 3)
+union(parent, rank, 1, 4)
+
+print(find(parent, 3))
+print(parent)
